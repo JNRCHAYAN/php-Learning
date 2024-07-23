ac595434c0414652f25b80b74599a9ee69708fab
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-23 21:22:35 +0600

    Update my css

--- Learning php/Project 1/submit.php
+++ Learning php/Project 1/submit.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>You Data</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="myst.css">
 </head>
 <body>
-    <div class="mainbody">
+    <div class="mainsubmit">
+        <h1>Thanks For fill up this from</h1>
+        <p>------------------------------</p>
+        <h3><i>Your Information here :</i> </h3>
     <?php
     $firstname =$_POST['FirstName'];
     $LastName =$_POST['LastName'];
@@ -16,7 +19,14 @@
     $FullAddress =$_POST['FullAddress'];
     $city =$_POST['city'];
 
-  
+    if(empty($firstname) ||empty( $LastName) || empty($email) || empty($phone) || empty($FullAddress) ||  empty($city) )
+    {
+        echo "Please Fill all the details";
+
+    }
+    else
+    {
+   
     $fullname = $firstname . " " .$LastName;
 
     echo "Your Full Name : " .$fullname;
@@ -29,7 +39,10 @@
      echo "<br>";
     echo "Your Address     : ". $FullAddress;
      echo "<br>";
+    } 
     ?>
+         
+  
 </div>
 </body>
 </html>
