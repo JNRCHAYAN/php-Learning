5ef5727a79d9873f791cfb60723dfe335e54f81f
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-23 15:44:56 +0600

    Donw

--- Learning php/Project 1/submit.php
+++ Learning php/Project 1/submit.php
@@ -4,24 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>You Data</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <div class="mainbody">
     <?php
     $firstname =$_POST['FirstName'];
     $LastName =$_POST['LastName'];
-    $phone =$_POST['phone'];
     $email =$_POST['email'];
-  
+    $phone =$_POST['number'];
+    $FullAddress =$_POST['FullAddress'];
+    $city =$_POST['city'];
 
+  
     $fullname = $firstname . " " .$LastName;
 
     echo "Your Full Name : " .$fullname;
-    if($phone)
-    {
-        echo "Your Contact   : " .$phone;
-    }
- 
-
+    echo "<br>";
+    echo "Your Email     : ". $email;
+     echo "<br>";
+    echo "Your Phone     : ". $phone;
+     echo "<br>";
+    echo "Your City     : ". $city;
+     echo "<br>";
+    echo "Your Address     : ". $FullAddress;
+     echo "<br>";
     ?>
+</div>
 </body>
 </html>
