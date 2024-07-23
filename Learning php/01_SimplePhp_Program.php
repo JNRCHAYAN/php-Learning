b318ce655061b3bd626a0024438561ccda368071
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-23 11:29:04 +0600

    Php Learn

---
+++ Learning php/01_SimplePhp_Program.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $today = date('Y-m-d');
+    echo $today;
+    echo" <br>";  
+    echo" <br>";
+    print  "Today date is $today";
+    $time = date('h:g:s');
+    print "<p>Today Time is $time</p>";
+
+    $date_and_time = date('Y-m-d h:g:s');
+    echo" <br>";
+    echo  $date_and_time;
+ 
+    ?>
+</body>
+</html>
