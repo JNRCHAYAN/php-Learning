e8c65b75dd25f5e92c8dbd46f50b55d4ed2fa1f9
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-23 11:40:39 +0600

    from desing with php

--- Learning php/02_HtmlFrom.php
+++ Learning php/02_HtmlFrom.php
@@ -4,8 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HTML FROM</title>
+    <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
-    <h3>Whats is your name</h3>
+    <h3>What's is your name</h3>
+    <form action="03_submit_from.php" method="post">
+        <label for="name">Name</label>
+        <br>
+        <input type="text" name="firstName" size="30">
+        <br>
+        <br>
+        <input type="submit" name="submit" id="submit" value="submit information">
+        <br>
+
+    </form>
 </body>
 </html>
