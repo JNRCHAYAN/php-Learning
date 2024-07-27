bd51322e710370e705296abd071ee88ef5c9cbb2
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-27 11:37:45 +0600

    Connection mysql

--- MySql PHP/02_Connection.php
+++ MySql PHP/02_Connection.php
@@ -6,13 +6,15 @@ $db = 'myfrom';
 
 $con = mysqli_connect($server,$username,$password,$db);
 
-if(!$con)
+if($con)
 {
     echo "Connection Successful";
 }
 else
 {
  echo "No Connection";
+ die("No connection" .mysqli_connect_error());
+
 }
 
 
