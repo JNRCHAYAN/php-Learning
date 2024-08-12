f3b26387a473b7d18fd1a17cd12a15e68e0be9d9
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-12 10:28:39 +0600

    setcookie

--- MySql PHP/08_Cookies.php
+++ MySql PHP/08_Cookies.php
@@ -1,5 +1,24 @@
 <?php
-setcookie('mylife' , 'chayan');
 
+// ** Set Cookie ****
+// setcookie('mylife' , 'chayan');
+setcookie('mylife' , 'chayan', time()+600);
 // setcookie(name,value,expire,path,domain,secure,httponly);   Cookie basic staucture **must be use name and value
+
+
+// ** Read Cookie
+echo $_COOKIE['mylife'];
+
+// ** Update Cookie 
+setcookie('mylife' , 'Roni', time()+600);
+
+
+// ** Read Cookie
+echo $_COOKIE['mylife'];
+
+// ** Delete Cookie
+setcookie('mylife','NULL', time()-600);  // when we can use negative time then the cookie is delete 
+
+
+
 ?>
