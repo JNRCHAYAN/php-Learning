a0839ed603f03b9449366b0085ba8b2ec70f2546
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-12 07:04:10 +0600

    cost

--- MySql PHP/06_Secure_Password.php
+++ MySql PHP/06_Secure_Password.php
@@ -2,7 +2,10 @@
 
 <?php
 $pas = "chayan";
-$strong_Pass = password_hash($pas,PASSWORD_BCRYPT);
+$option = ['cost'=> 12];  // It's mean how many time needs to create a password with this algotithm 
+
+// $strong_Pass = password_hash($pas,PASSWORD_BCRYPT);
+$strong_Pass = password_hash($pas,PASSWORD_BCRYPT,$option);
 echo $strong_Pass;
 
 $pass_Chack = password_verify($pas,$strong_Pass);
